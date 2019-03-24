6561625377c0290eb5e04dd9af2b3e56e2278fb9
temaand1 <45510216+temaand1@users.noreply.github.com>
2019-03-24 16:26:18 +0300

    Done!

--- src/index.js
+++ src/index.js
@@ -1,3 +1,34 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+    var chars = str.split(''),
+        bracketsConfig = [],
+        open = ['{', '(', '['],
+        close = ['}', ')', ']'],
+        closeIndex,
+        openIndex;
+
+    // Проходимся по строке, проверяя каждый ее символ (п.4).
+    for (var i = 0, len = chars.length; i < len; i++) {
+        openIndex = open.indexOf(chars[i]);
+        if (openIndex !== -1) {
+            // Нашли открывающую скобку. Помещаем ее в стек (п.2).
+            bracketsConfig.push(openIndex);
+            continue;
+        }
+
+        closeIndex = close.indexOf(chars[i]);
+        if (closeIndex !== -1) {
+            // Нашли закрывающую скобку. Проверяем ее соответствие открывающей (п.3).
+            openIndex = bracketsConfig.pop();
+            if (closeIndex !== openIndex) {
+                return false;
+            }
+        }
+    }
+
+    // Проверяем дисбаланс открытых/закрытых скобок (п.5).
+    if (bracketsConfig.length !== 0) {
+        return false;
+    }
+
+    return true;
 }
